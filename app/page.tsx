c22ec6fddb6fc76eef1c54409ed4d459e0d261f3
remus00 <135813551+remus00@users.noreply.github.com>
2024-02-03 18:39:26 +0100

    Project-structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { CollaborativeApp } from './CollaborativeApp';
-import { Room } from './Room';
-
 export default function Page() {
     return (
-        <Room>
-            <CollaborativeApp />
-        </Room>
+        <div className="h-[100vh] w-full flex justify-center items-center text-center">
+            <h1 className="font-2xl text-white">Figma Clone</h1>
+        </div>
     );
 }
